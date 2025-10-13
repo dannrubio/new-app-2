b315778b73a14fa53b57ccf69b20fe6cd41de560
dannrubio <dann.rubio@gmail.com>
2025-10-13 12:45:17 +0800

    Testing the change of globals.css location

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './globals.css';
+import '../styles/globals.css';
 import Header from '../components/Header';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
